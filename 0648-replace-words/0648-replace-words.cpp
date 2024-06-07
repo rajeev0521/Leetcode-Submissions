6bdd12edfbd3a05317d640a1d002c3d11296c4b0
Rajeev Gupta <143323380+rajeev0521@users.noreply.github.com>
2024-06-08 01:25:40 +0530

    Time: 81 ms (67.73%), Space: 72.6 MB (69.27%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,26 +1,66 @@
+class Trie {
+public:
+    Trie* children[26];
+    bool isEnd;
+
+    Trie() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+        isEnd = false;
+    }
+};
+
 class Solution {
 public:
-    string replaceWords(vector<string>& dict, string sentence) {
-        unordered_set<string> s(begin(dict), end(dict));
-        istringstream iss(sentence);
-        vector<string> temp;
-        string word, res; 
+    Trie* root;
 
-        while(iss >> word) temp.push_back(word);
-        for(string t : temp) {
-            int i = 0;
-            while(i++ <= t.length()) {
-                string curr = t.substr(0, i);
-                if(s.find(curr) != s.end()) {
-                    res += curr + " ";
-                    break;
-                }
-                
-                if(i == t.length()) res += curr + " ";
+    Solution() {
+        root = new Trie();
+    }
+
+    void insert(string word) {
+        Trie* node = root;
+        for (char c : word) {
+            int i = c - 'a';
+            if (node->children[i] == nullptr) {
+                node->children[i] = new Trie();
             }
+            node = node->children[i];
+        }
+        node->isEnd = true;
+    }
+
+    string search(string word) {
+        Trie* node = root;
+        string result;
+        for (char c : word) {
+            int i = c - 'a';
+            if (node->children[i] == nullptr) {
+                return word;
+            }
+            result += c;
+            if (node->children[i]->isEnd) {
+                return result;
+            }
+            node = node->children[i];
+        }
+        return word;
+    }
+
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        for (string word : dictionary) {
+            insert(word);
         }
 
-        res.erase(res.size() - 1);
-        return res;
+        stringstream ss(sentence);
+        string word, result;
+        while (ss >> word) {
+            if (!result.empty()) {
+                result += " ";
+            }
+            result += search(word);
+        }
+        return result;
     }
 };
